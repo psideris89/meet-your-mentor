042d643ed8fe10f7292b185f943e817bdb226739
Panos <panos.sideris@bango.com>
2022-12-26 10:58:26 +0000

    Remove unused imports

--- src/main/java/com/psideris/meetyourmentor/model/Mentee.java
+++ src/main/java/com/psideris/meetyourmentor/model/Mentee.java
@@ -1,7 +1,6 @@
 package com.psideris.meetyourmentor.model;
 
 import com.psideris.meetyourmentor.model.dto.MenteeDto;
-import com.psideris.meetyourmentor.model.dto.MentorDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/com/psideris/meetyourmentor/repository/MenteeRepository.java
+++ src/main/java/com/psideris/meetyourmentor/repository/MenteeRepository.java
@@ -1,7 +1,6 @@
 package com.psideris.meetyourmentor.repository;
 
 import com.psideris.meetyourmentor.model.Mentee;
-import com.psideris.meetyourmentor.model.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
