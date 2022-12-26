254d56a3d8cf61240893ee4b305835ef1c437df7
Panos <panos.sideris@bango.com>
2022-12-26 11:52:55 +0000

    Front-end app routing

---
+++ webapp/app/routes/courses/index.tsx
@@ -0,0 +1,7 @@
+export default function Courses() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Our Courses</h1>
+    </div>
+  );
+}
--- webapp/app/routes/index.tsx
+++ webapp/app/routes/index.tsx
@@ -1,30 +1,25 @@
+import {Link} from "@remix-run/react";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Meet Your Mentor</h1>
+      <p>This is the place to connect with people who are interested either in mentoring or being mentored</p>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <Link to="/mentors">
+            I am a mentor!
+          </Link>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+          <Link to="/mentees">
+            I want to find a mentor!
+          </Link>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <Link to="/courses">
+            Our courses
+          </Link>
         </li>
       </ul>
     </div>
